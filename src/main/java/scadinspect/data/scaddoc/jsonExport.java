053790ab05e24568694b3cd8971bb563cdadb595
Buckwich <s.steinruecken@gmx.net>
2017-03-17 16:39:48 +0100

    support for list of parts

--- src/main/java/scadinspect/data/scaddoc/jsonExport.java
+++ src/main/java/scadinspect/data/scaddoc/jsonExport.java
@@ -1,6 +1,8 @@
 package scadinspect.data.scaddoc;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import org.json.*;
 import scadinspect.data.scaddoc.properties.*;
 import scadinspect.data.scaddoc.properties.Property;
@@ -16,18 +18,22 @@ public class jsonExport {
     jsonExport export = new jsonExport();
     System.out.println("test");
     Module test = new Module();
-    test.addProperty(new IntSingleProperty("Singlel Int", 20));
+    test.addProperty(new IntSingleProperty("Single Int", 20));
     test.addProperty(new RealSingleProperty("Single Flaot", 123.33));
     test.addProperty(new IntMultiProperty("Multiple Int", 1l, 2l, 3l));
     test.addProperty(new RealMultiProperty("Multiple Float", 1.0, 2.6));
     test.addProperty(new PairProperty("Pair", new Pair(100, "EUR")));
     test.addProperty(new StringSingleProperty("Single String", "test"));
     test.addProperty(new StringMultiProperty("Multiple Strings", "abc", "def"));
-    System.out.println(export.oneLine(test));
+
+    List<Module> multuTest=new ArrayList<>();
+    multuTest.add(test);
+    multuTest.add(test);
+    System.out.println(export.multiLine(multuTest));
   }
 
-  public String oneLine(Module part) {
-    JSONArray module = new JSONArray();
+  public JSONObject oneLine(Module part) {
+
     JSONObject element = new JSONObject();
     for (Property property : part.getProperties()) {
       if (Pair.class.isInstance(property.getValue())) {
@@ -40,9 +46,18 @@ public class jsonExport {
         element.accumulate(property.getKey(), property.getValue());
       }
     }
-    module.put(element);
-    return module.toString();
+
+    return element;
   }
+
+  public String multiLine(List<Module> parts) {
+    JSONArray list=new JSONArray();
+    for (Module part:parts){
+      list.put(oneLine(part));
+    }
+    return list.toString();
+  }
+
 }
 
 
