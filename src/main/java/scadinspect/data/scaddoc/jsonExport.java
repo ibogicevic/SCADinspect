05d54d6bbfe92e5abddf25623a668986a237ea0d
Buckwich <s.steinruecken@gmx.net>
2017-03-17 16:04:06 +0100

    wip json export

--- src/main/java/scadinspect/data/scaddoc/jsonExport.java
+++ src/main/java/scadinspect/data/scaddoc/jsonExport.java
@@ -1,11 +1,9 @@
 package scadinspect.data.scaddoc;
 
-import java.util.List;
-import jdk.nashorn.internal.runtime.JSONListAdapter;
 import org.json.*;
-import scadinspect.data.scaddoc.properties.*;
 import scadinspect.data.scaddoc.properties.Property;
-import scadinspect.data.scaddoc.properties.impl.IntSingleProperty;
+import scadinspect.data.scaddoc.properties.impl.*;
+
 
 /**
  * Created by simon on 3/17/17.
@@ -17,13 +15,21 @@ public class jsonExport {
     System.out.println("test");
     Module test = new Module();
     test.addProperty(new IntSingleProperty("amount", 20));
+    test.addProperty(new RealSingleProperty("amount", 24.9));
     System.out.println(export.oneLine(test));
   }
 
   public String oneLine(Module part) {
     JSONArray module = new JSONArray();
     for (Property property : part.getProperties()) {
+
+      System.out.println(property.getKey()+"\t\t"+property.getValue());
       System.out.println(property);
+      System.out.println();
+
+      JSONObject element =new JSONObject();
+      element.append(property.getKey(),property.getValue());
+      module.put(element);
     }
     return module.toString();
   }
