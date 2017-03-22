08ef1688d8982e96a2a486b521515079be7c603b
Til Goepfert <tilgoepfert@gmail.com>
2017-03-22 16:57:26 +0100

    added working toString method

--- src/main/java/scadinspect/data/scaddoc/XmlExport.java
+++ src/main/java/scadinspect/data/scaddoc/XmlExport.java
@@ -4,10 +4,17 @@ package scadinspect.data.scaddoc;
  * Created by eric on 22.03.17.
  */
 
+import java.io.StringWriter;
+import java.util.Collection;
 import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -29,20 +36,15 @@ public class XmlExport {
 
     // every single module into one element
     for (Module module : modules) {
-      rootElement.appendChild(singleModule(module));
+      rootElement.appendChild(singleModule(doc, module));
     }
 
-    return doc.toString();
+    return exportString(doc);
 
   }
 
-  private Element singleModule(Module module) throws ParserConfigurationException {
+  private Element singleModule(Document doc, Module module) throws ParserConfigurationException {
 
-    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
-    // root elements
-    Document doc = docBuilder.newDocument();
     Element moduleNode = doc.createElement("module");
 
     List<Property> properties = module.getProperties();
@@ -54,7 +56,7 @@ public class XmlExport {
       // if it is MultiProperty, create node for every value
       if (property instanceof MultiProperty) {
         MultiProperty multiProperty = (MultiProperty) property;
-        List values = multiProperty.getValue();
+        Collection values = multiProperty.getValue();
         for (Object value : values) {
           Element subValue = doc.createElement(multiProperty.getKey() + "Value");
           subValue.appendChild(doc.createTextNode(value.toString()));
@@ -84,5 +86,22 @@ public class XmlExport {
       moduleNode.appendChild(key);
     }
     return moduleNode;
+  }
+
+  private String exportString(Document doc) {
+    try {
+      StringWriter sw = new StringWriter();
+      TransformerFactory tf = TransformerFactory.newInstance();
+      Transformer transformer = tf.newTransformer();
+      transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+      transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+      transformer.transform(new DOMSource(doc), new StreamResult(sw));
+      return sw.toString();
+    } catch (Exception ex) {
+      throw new RuntimeException("Error converting to String", ex);
+    }
   }
 }
