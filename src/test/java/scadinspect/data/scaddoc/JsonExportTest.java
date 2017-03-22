6dbe43b32921228da00e63cd61897b33c24ead76
Buckwich <s.steinruecken@gmx.net>
2017-03-22 15:04:25 +0100

    unit tests for json export

--- src/test/java/scadinspect/data/scaddoc/JsonExportTest.java
+++ src/test/java/scadinspect/data/scaddoc/JsonExportTest.java
@@ -1,5 +1,8 @@
 package scadinspect.data.scaddoc;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.LinkedList;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
@@ -15,47 +18,112 @@ import scadinspect.data.scaddoc.properties.helper.Pair;
  */
 class JsonExportTest {
 
-  JsonExport exporter;
-  List<Module> modules;
+  private JsonExport exporter;
+  private List<Module> modules;
 
   @BeforeEach
-  public void instanciate() {
+  void instanciate() {
     exporter = new JsonExport();
     modules = new LinkedList<>();
   }
 
   @AfterEach
-  public void output() {
+  void output() {
     System.out.println(exporter.multiLine(modules));
   }
 
   @Test
   void emptyList() {
+    assertEquals("[]",exporter.multiLine(modules));
   }
 
+  @Test
+  void singlePropertyInt(){
+    Module singleProperty = new Module();
+    singleProperty.addProperty(new SingleProperty<>("key",1));
+    modules.add(singleProperty);
+    assertEquals("[{\"key\":1}]",exporter.multiLine(modules));
+  }
+  @Test
+  void singlePropertyFloat(){
+    Module singleProperty = new Module();
+    singleProperty.addProperty(new SingleProperty<>("key",0.1));
+    modules.add(singleProperty);
+    assertEquals("[{\"key\":0.1}]",exporter.multiLine(modules));
+  }
+  @Test
+  void singlePropertyString(){
+    Module singleProperty = new Module();
+    singleProperty.addProperty(new SingleProperty<>("key","value"));
+    modules.add(singleProperty);
+    assertEquals("[{\"key\":\"value\"}]",exporter.multiLine(modules));
+  }
+  @Test
+  void singlePropertyToString(){
+    assertEquals("key: value",new SingleProperty<>("key","value").toString());
+
+  }
+  @Test
+  void multiPropertyInt(){
+    Module multiProperty = new Module();
+    multiProperty.addProperty(new MultiProperty<>("key",1,2,3));
+    modules.add(multiProperty);
+    assertEquals("[{\"key\":[1,2,3]}]",exporter.multiLine(modules));
+  }
+  @Test
+  void multiPropertyFloat(){
+    Module multiProperty = new Module();
+    multiProperty.addProperty(new MultiProperty<>("key",0.1,1.1,2,4));
+    modules.add(multiProperty);
+    assertEquals("[{\"key\":[0.1,1.1,2,4]}]",exporter.multiLine(modules));
+  }
+  @Test
+  void multiPropertyString(){
+    Module multiProperty = new Module();
+    multiProperty.addProperty(new MultiProperty<>("key","a","b","c"));
+    modules.add(multiProperty);
+    assertEquals("[{\"key\":[\"a\",\"b\",\"c\"]}]",exporter.multiLine(modules));
+  }
+  @Test
+  void multiPropertyList(){
+    Module multiProperty = new Module();
+    List property=new LinkedList<String>();
+    property.add("a");
+    property.add("b");
+    property.add("c");
+    multiProperty.addProperty(new MultiProperty("key",property));
+    modules.add(multiProperty);
+    assertEquals("[{\"key\":[\"a\",\"b\",\"c\"]}]",exporter.multiLine(modules));
+  }
+  @Test
+  void multiPropertyToString(){
+    assertEquals("key: [a, b]",new MultiProperty<>("key","a","b").toString());
+
+  }
 //  @Test
-//  void oneLine() {
-//    Module mod1 = new Module();
+//  void pairPropertyInt(){
+//    Module pairProperty = new Module();
+//    pairProperty.addProperty(new PairProperty<>("key","12"));
+//    modules.add(pairProperty);
+//    assertEquals("[{\"key\":[1,2,3]}]",exporter.multiLine(modules));
+//  }
+//  @Test
+//  void pairPropertyFloat(){
+//    Module pairProperty = new Module();
+//    pairProperty.addProperty(new PairProperty<>("key",0.1,1.1,2,4));
+//    modules.add(pairProperty);
+//    assertEquals("[{\"key\":[0.1,1.1,2,4]}]",exporter.multiLine(modules));
+//  }
+//  @Test
+//  void pairPropertyString(){
+//    Module pairProperty = new Module();
+//    pairProperty.addProperty(new PairProperty<>("key","a","b","c"));
+//    modules.add(pairProperty);
+//    assertEquals("[{\"key\":[\"a\",\"b\",\"c\"]}]",exporter.multiLine(modules));
+//  }
+//    @Test
+//  void pairPropertyToString(){
+//    assertEquals("key: [a, b]",new PairProperty<>("key","a","b").toString());
 //
-//    mod1.addProperty(new SingleProperty<>("part", "Wheel"));
-//    mod1.addProperty(new PairProperty("price", new Pair(100, "EUR")));
-//    mod1.addProperty(new SingleProperty<>("amount", 4));
-//    mod1.addProperty(new PairProperty("weight", new Pair(12, "kg")));
-//    mod1.addProperty(new MultiProperty<>("materials", "Rubber", "Aluminium"));
-//    mod1.addProperty(new SingleProperty<>("url", "https://example.com"));
-//    mod1.addProperty(new SingleProperty<>("<key>", "<value"));
-//
-//    Module mod2 = new Module();
-//
-//    mod2.addProperty(new SingleProperty<>("part", "Motor"));
-//    mod2.addProperty(new PairProperty("price", new Pair(1000, "USD")));
-//    mod2.addProperty(new SingleProperty<>("amount", 1));
-//    mod2.addProperty(new PairProperty("weight", new Pair(200, "kg")));
-//    mod2.addProperty(new SingleProperty<>("materials", "Steel"));
-//    mod2.addProperty(new SingleProperty<>("url", "https://example.com"));
-//    mod2.addProperty(new SingleProperty<>("<key>", "<value>"));
-//
-//    modules.add(mod1);
-//    modules.add(mod2);
 //  }
 }
