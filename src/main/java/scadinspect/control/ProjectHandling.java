af14c1dab4a422978252344f7f951164c12f0a67
bilir <bilir@FAA2GSJYG4.emea.hpqcorp.net>
2017-03-23 15:21:10 +0100

    Added fileList, fixed issue where only the path, but not files were opened

--- src/main/java/scadinspect/control/ProjectHandling.java
+++ src/main/java/scadinspect/control/ProjectHandling.java
@@ -1,9 +1,8 @@
 package scadinspect.control;
 
 import java.io.File;
-
-import javax.swing.JFileChooser;
-import javax.swing.UIManager;
+import java.io.FileFilter;
+import java.util.ArrayList;
 
 import scadinspect.gui.Main;
 import javafx.stage.DirectoryChooser;
@@ -11,13 +10,73 @@ import javafx.stage.FileChooser;
 
 public class ProjectHandling {
 	
-	// Definiton of the Chooser
+	/**
+	 * TODO: 
+	 */
+	
+	// Definiton of the chooser
 	private final DirectoryChooser directoryChooser = new DirectoryChooser();
 	private final FileChooser fileChooser = new FileChooser();
-
+	
+	// Filter for the chooser
+	private final FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("SCAD files", "*.scad");
+	private final FileFilter fileFilter= new FileFilter() {
+		@Override
+		/**
+		 * Accepts only files which end with .scad
+		 */
+		public boolean accept(File file) {
+			if(file.getName().toLowerCase().endsWith(".scad")){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	};
+	
 	// Definition of the variables
 	private File projectFile;
 	private File projectDirectory;
+	private ArrayList<File> fileList = new ArrayList();
+	
+	/**
+	 * Constructor
+	 */
+	public ProjectHandling(){
+		fileChooser.getExtensionFilters().add(extensionFilter);
+	}
+	
+	/**
+	 * Opens the dialog to choose a file
+	 */
+	public void openProjectFile() {
+		projectFile = fileChooser.showOpenDialog(Main.getInstance().getPrimaryStage());
+		setProjectPath(projectFile);
+		if(projectFile!=null)
+			fileList.add(projectFile);
+	}
+	
+	/**
+	 * Opens the dialog to choose a directory
+	 */
+	public void openProjectFolder(){
+		projectDirectory = directoryChooser.showDialog(Main.getInstance().getPrimaryStage());
+		setProjectPath(projectDirectory);
+		if(projectDirectory!=null)
+			addFilesToList(projectDirectory);
+	}
+	
+	/**
+	 * Checks if a path for the project is set, if so it sets the pathname in the title and opens the project
+	 * @param projectPath
+	 */
+	private void setProjectPath(File projectPath){
+		if (projectPath != null) {
+			projectPath.getAbsolutePath();
+			setCurrentProject(projectPath.getAbsolutePath().toString());
+			Main.getInstance().toolbarArea.disableButtons(false);
+		}
+	}
 	
 	/**
 	 * Sets the current project path in the Title and the main class
@@ -31,33 +90,15 @@ public class ProjectHandling {
 	}
 
 	/**
-	 * Opens the dialog to choose a file
-	 */
-	public void openProjectFile() {
-		projectFile = fileChooser.showOpenDialog(Main.getInstance().getPrimaryStage());
-		setProjectPath(projectFile);
-	}
-	
-	/**
-	 * Opens the dialog to choose a directory
-	 */
-	public void openProjectFolder(){
-		projectDirectory = directoryChooser.showDialog(Main.getInstance().getPrimaryStage());
-		setProjectPath(projectDirectory);
-	}
-	
-	/**
-	 * Checks if a path for the project is set, if so it sets the pathname in the title and opens the project
+	 * Gets the files in the current directory and adds only .scad files to the list
 	 * @param projectDirectory
 	 */
-	private void setProjectPath(File projectPath){
-		if (projectPath != null) {
-			projectPath.getAbsolutePath();
-			setCurrentProject(projectPath.getAbsolutePath().toString());
-			Main.getInstance().toolbarArea.disableButtons(false);
+	private void addFilesToList(File projectDirectory) {
+		for(File file:projectDirectory.listFiles(fileFilter)){
+			fileList.add(file);
 		}
 	}
-
+	
 	/**
 	 * Closes the current project and removes the path from the title
 	 */
@@ -66,7 +107,16 @@ public class ProjectHandling {
 		if(Main.getInstance().isProjectOpen()==true){
 			Main.getInstance().setCurrentProject("");
 			Main.getInstance().getPrimaryStage().setTitle(Main.APPNAME);
+			fileList.clear();
 		}
+	}
+	
+	/**
+	 * Returns the currently opened Files
+	 * @return fileList
+	 */
+	public ArrayList<File> getFileList(){
+		return fileList;
 	}
 
 }
