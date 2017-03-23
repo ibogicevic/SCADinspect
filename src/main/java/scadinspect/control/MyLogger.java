ebff88dc7e477e07343670176274a08755435d1c
Niklas Sauer <nik.sauer@me.com>
2017-03-23 14:52:56 +0100

    code

--- src/main/java/scadinspect/control/MyLogger.java
+++ src/main/java/scadinspect/control/MyLogger.java
@@ -1,9 +1,25 @@
 package scadinspect.control;
 
+import java.io.IOException;
+import java.util.logging.*;
+
 /**
  * Created by nik on 23.03.17.
  */
 public class MyLogger {
+    public static void setup() throws IOException {
+        // get and configure global logger
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        logger.setLevel(Level.INFO);
 
-  //hi
+        // suppress console output
+        Logger rootLogger = Logger.getLogger("");
+        Handler[] handlers = rootLogger.getHandlers();
+        if (handlers[0] instanceof ConsoleHandler) {
+            rootLogger.removeHandler(handlers[0]);
+        }
+
+        Handler logAll = new FileHandler("logAll.txt");
+        logger.addHandler(logAll);
+    }
 }
