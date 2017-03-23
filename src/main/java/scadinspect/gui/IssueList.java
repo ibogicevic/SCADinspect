2d6480c4959f6e0bc4cc4dcd2b2df8b9b5ed0c97
EvilPandaBear <tom.halecker@gmx.net>
2017-03-23 15:19:08 +0100

    progress of issuelsit

--- src/main/java/scadinspect/gui/IssueList.java
+++ src/main/java/scadinspect/gui/IssueList.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.util.ArrayList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -7,14 +8,39 @@ import javafx.scene.control.TableView;
  * Created by halecket on 17.03.2017.
  */
 public class IssueList {
-  TableView issueList = new TableView();
+  TableView<String> issueList = new TableView();
   TableColumn issues = new TableColumn("issues");
+  TableColumn ressources = new TableColumn("ressources");
 
   public TableView showList () {
-    issues.setPrefWidth(100);
     issueList.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-    issueList.getColumns().add(issues);
+    issueList.getColumns().addAll(issues, ressources);
+
+
+
     return issueList;
+  }
+
+  ArrayList dummy = new ArrayList();
+
+  /*
+  addDataToTable gets an array list with issue objects.
+  a for loop circles through the list
+  issue objects get added to row
+  */
+
+  public void addDataToTable () {
+    for(int i = 0; i < dummy.size(); i++){
+      String tempLineNumber = new String(dummy.get(i).lineNumber.toString);
+      String tempIsError
+      if(dummy.get(i).isError){
+        tempIsError = "Error";
+      } else {
+        tempIsError = "Warning";
+      }
+
+      issueList.getItems(tempIsError + " " + dummy.get(i).sourceFile + " " + tempLineNumber + " " + dummy.get(i).issueIdentifier + " " + dummy.get(i).description);
+    }
   }
 
   //TO DO get data
