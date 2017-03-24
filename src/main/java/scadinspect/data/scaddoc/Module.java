427e1cca030ea3ed30f932ab42f085173e2f1aa5
Buckwich <s.steinruecken@gmx.net>
2017-03-24 15:57:24 +0100

    /docuParser: fixed issues with junit and collections

--- src/main/java/scadinspect/data/scaddoc/Module.java
+++ src/main/java/scadinspect/data/scaddoc/Module.java
@@ -30,12 +30,46 @@ public class Module {
 
   @Override
   public String toString() {
-    String res= "{";
-    for (Property property: properties) {
-      res+=property.toString()+"\n";
+    String res = "{";
+    for (Property property : properties) {
+      res += property.toString() + "\n";
     }
-    res+="}";
+    res += "}";
     return res;
   }
 
+  @Override
+  public boolean equals(Object other) {
+    if (other == null) {
+      return false;
+    }
+    if (other == this) {
+      return true;
+    }
+    if (!(other instanceof Module)) {
+      return false;
+    }
+
+    Collection<String> stringOther = new HashSet<>();
+    for (Property property : ((Module) other).getProperties()) {
+      stringOther.add(property.toString());
+    }
+    Collection<String> stringThis = new HashSet<>();
+    for (Property property : properties) {
+      stringThis.add(property.toString());
+    }
+    for (String stringProperty : stringThis) {
+      if (!stringOther.contains(stringProperty)) {
+        System.out.println("Error In This: " + stringProperty.toString());
+        return false;
+      }
+    }
+    for (String stringProperty : stringOther) {
+      if (!stringThis.contains(stringProperty)) {
+        System.out.println("Error In Other: " + stringProperty.toString());
+        return false;
+      }
+    }
+    return true;
+  }
 }
--- src/test/java/scadinspect/data/scaddoc/parser/PropertyParserTest.java
+++ src/test/java/scadinspect/data/scaddoc/parser/PropertyParserTest.java
@@ -1,17 +1,12 @@
 package scadinspect.data.scaddoc.parser;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Collection;
-import java.util.HashSet;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import scadinspect.data.scaddoc.Module;
 import scadinspect.data.scaddoc.properties.MultiProperty;
 import scadinspect.data.scaddoc.properties.PairProperty;
-import scadinspect.data.scaddoc.properties.Property;
 import scadinspect.data.scaddoc.properties.SingleProperty;
-
 
 /**
  * @author simon, tom on 3/23/17.
@@ -30,25 +25,17 @@ class PropertyParserTest {
         + "* @comment So könnte das Ganze funktionieren..."
         + "*/";
     propertyParser = new PropertyParser(content);
+
     Module output = new Module();
     output.addProperty(new SingleProperty<>("title", "Module_1"));
     output.addProperty(new PairProperty<>("price", "100", "EUR"));
     output.addProperty(new MultiProperty<>("material", "wood", "metal"));
     output.addProperty(new SingleProperty<>("comment", "So könnte das Ganze funktionieren..."));
-    System.out.println(propertyParser.parseModules());
-    Collection<Property> parsed = propertyParser.parseModules().iterator().next().getProperties();
-    Collection<String> stringParsed = new HashSet<>();
-    for (Property property : parsed) {
-      stringParsed.add(property.toString());
-    }
-    Collection<String> stringOutput = new HashSet<>();
-    for (Property property : output.getProperties()) {
-      stringOutput.add(property.toString());
-    }
-    boolean isEqual = true;
-    for (String propertyString : stringOutput) {
-      isEqual &= stringParsed.contains(propertyString);
-    }
-    assertEquals(true, isEqual);
+
+    Module parsed = propertyParser.parseModules().iterator().next();
+
+    assertEquals(output,parsed);
+
+
   }
 }
