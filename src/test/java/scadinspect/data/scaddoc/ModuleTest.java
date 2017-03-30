d36ff0b7c7a7a3d10253ed4f410276470714daed
Tom Richter <tom.richter@hpe.com>
2017-03-30 13:59:14 +0200

    Module Tests

---
+++ src/test/java/scadinspect/data/scaddoc/ModuleTest.java
@@ -0,0 +1,19 @@
+package scadinspect.data.scaddoc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import scadinspect.data.scaddoc.properties.SingleProperty;
+
+/**
+ * Created by richteto on 30.03.2017.
+ */
+class ModuleTest {
+
+  @Test
+  void toStringTest() {
+    Module module = new Module();
+    module.addProperty(new SingleProperty<>("a", 123));
+    assertEquals("{a: 123\n}", module.toString());
+  }
+}
