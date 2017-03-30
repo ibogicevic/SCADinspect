7bb83e50678e69008c5f857efa8b88e0a6ebb6d0
Philipp Grosskreutz <philipp@grossx.de>
2017-03-30 16:24:58 +0200

    added Documentation class and TableColumn of DocumentationList

---
+++ src/main/java/scadinspect/data/scaddoc/Documentation.java
@@ -0,0 +1,13 @@
+package data.scaddoc;
+
+import javax.swing.text.Document;
+import java.util.ArrayList;
+
+/**
+ * Created by Großkreutz on 30.03.2017.
+ */
+public class Documentation {
+    String part, material, url;
+    Double weight, price;
+    Integer amount;
+}
--- src/main/java/scadinspect/gui/DocumentationList.java
+++ src/main/java/scadinspect/gui/DocumentationList.java
@@ -1,19 +1,42 @@
 package scadinspect.gui;
 
+import data.scaddoc.Documentation;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-/**
- * Created by halecket on 17.03.2017.
- */
-public class DocumentationList {
-  TableView documentationList = new TableView();
-  TableColumn documentation = new TableColumn("Documentation");
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-  public TableView showList () {
-    documentation.setPrefWidth(100);
-    documentationList.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-    documentationList.getColumns().add(documentation);
-    return documentationList;
+public class DocumentationList {
+  //TableView of Documentation List
+  private TableView docList = new TableView();
+
+  //Data structure with associated data for documentation List
+  private final ObservableList<Documentation> docData = FXCollections.observableArrayList();
+
+  private TableColumn part = new TableColumn("Part");
+  private TableColumn price = new TableColumn("Price");
+  private TableColumn amount = new TableColumn("Amount");
+  private TableColumn weight = new TableColumn("Weight");
+  private TableColumn material = new TableColumn("Material");
+  private TableColumn url = new TableColumn("URL");
+
+  public TableView generateList () {
+      //addData(); ---- dummy data currently missing
+
+
+      docList.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+      docList.setItems(docData);
+    docList.getColumns().addAll(part, price, amount, weight, material, url);
+    return docList;
+  }
+
+  public void addData (ArrayList<Documentation> documentations){
+      for(int i = 0; i < documentations.size(); i++){
+          docData.add(i, documentations.get(i));
+      }
   }
 }
